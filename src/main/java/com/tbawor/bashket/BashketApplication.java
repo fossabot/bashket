70a47d9b7466d74b80870c22d5d6ad300d2cd515
Tomasz Bawor <bawortomasz@gmail.com>
2018-02-01 11:33:14 +0100

    Initial commit

---
+++ src/main/java/com/tbawor/bashket/BashketApplication.java
@@ -0,0 +1,11 @@
+package com.tbawor.bashket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BashketApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(BashketApplication.class, args);
+  }
+}
---
+++ src/test/java/com/tbawor/bashket/BashketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tbawor.bashket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BashketApplicationTests {
+
+  @Test
+  public void shouldLoadContext() {
+  }
+
+}
